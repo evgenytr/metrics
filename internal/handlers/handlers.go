b314ac77a53d908ccad1474658fa1728b94925ac
evgenytr <evgenytr@gmail.com>
2023-06-13 15:46:19 +0300

    Adds empty body handling as not an error

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -231,6 +231,11 @@ func (h *StorageHandler) ProcessPostUpdatesBatchJSONRequest(res http.ResponseWri
 	}
 
 	fmt.Println(buf.String())
+	if buf.Len() == 0 {
+		res.Write([]byte("Empty body"))
+		res.WriteHeader(http.StatusOK)
+		return
+	}
 
 	err = json.Unmarshal(buf.Bytes(), &currMetrics)
 
