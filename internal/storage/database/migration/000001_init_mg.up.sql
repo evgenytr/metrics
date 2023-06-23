09d8fd2bf2d04b313ae1956057699cce521d7452
evgenytr <evgenytr@gmail.com>
2023-06-23 21:01:40 +0300

    Update migrations

--- internal/storage/database/migration/000001_init_mg.up.sql
+++ internal/storage/database/migration/000001_init_mg.up.sql
@@ -1,12 +1,3 @@
-CREATE USER evgeny
-    PASSWORD 'evgeny';
-
-CREATE DATABASE evgeny_trefilov
-    OWNER 'evgeny'
-    ENCODING 'UTF-8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8';
-
 CREATE TABLE IF NOT EXISTS metrics (
         id SERIAL PRIMARY KEY,
         metric_name VARCHAR(100) UNIQUE,
