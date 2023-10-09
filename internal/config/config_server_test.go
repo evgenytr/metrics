c3f87334f9f1ef93483c834b35163ea511d37996
evgenytr <evgenytr@gmail.com>
2023-10-09 20:56:36 +0300

    updates server config test

--- internal/config/config_server_test.go
+++ internal/config/config_server_test.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"reflect"
 	"testing"
 	"time"
 )
@@ -27,35 +26,35 @@ func TestGetServerConfig(t *testing.T) {
 			wantDBDSN:            "",
 			wantKey:              "",
 			wantCryptoKey:        "./rsakeys/private.pem",
-			wantTrustedSubnet:    "192.168.0.0/24",
+			wantTrustedSubnet:    "",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotHost, gotStoreIntervalOut, gotFileStoragePath, gotRestore, gotDBDSN, gotKey, gotCryptoKey, gotTrustedSubnet := GetServerConfig()
-			if !reflect.DeepEqual(gotHost, tt.wantHost) {
+			if gotHost != tt.wantHost {
 				t.Errorf("GetServerConfig() gotHost = %v, want %v", gotHost, tt.wantHost)
 			}
-			if !reflect.DeepEqual(gotStoreIntervalOut, tt.wantStoreIntervalOut) {
+			if gotStoreIntervalOut != tt.wantStoreIntervalOut {
 				t.Errorf("GetServerConfig() gotStoreIntervalOut = %v, want %v", gotStoreIntervalOut, tt.wantStoreIntervalOut)
 			}
-			if !reflect.DeepEqual(gotFileStoragePath, tt.wantFileStoragePath) {
+			if gotFileStoragePath != tt.wantFileStoragePath {
 				t.Errorf("GetServerConfig() gotFileStoragePath = %v, want %v", gotFileStoragePath, tt.wantFileStoragePath)
 			}
-			if !reflect.DeepEqual(gotRestore, tt.wantRestore) {
+			if gotRestore != tt.wantRestore {
 				t.Errorf("GetServerConfig() gotRestore = %v, want %v", gotRestore, tt.wantRestore)
 			}
-			if !reflect.DeepEqual(gotDBDSN, tt.wantDBDSN) {
+			if gotDBDSN != tt.wantDBDSN {
 				t.Errorf("GetServerConfig() gotDBDSN = %v, want %v", gotDBDSN, tt.wantDBDSN)
 			}
-			if !reflect.DeepEqual(gotKey, tt.wantKey) {
+			if gotKey != tt.wantKey {
 				t.Errorf("GetServerConfig() gotKey = %v, want %v", gotKey, tt.wantKey)
 			}
-			if !reflect.DeepEqual(gotCryptoKey, tt.wantCryptoKey) {
-				t.Errorf("GetAgentConfig() gotCryptoKey = %v, want %v", gotCryptoKey, tt.wantCryptoKey)
+			if gotCryptoKey != tt.wantCryptoKey {
+				t.Errorf("GetServerConfig() gotCryptoKey = %v, want %v", gotCryptoKey, tt.wantCryptoKey)
 			}
-			if !reflect.DeepEqual(gotTrustedSubnet, tt.wantTrustedSubnet) {
-				t.Errorf("GetAgentConfig() gotCryptoKey = %v, want %v", gotCryptoKey, tt.wantCryptoKey)
+			if gotTrustedSubnet != tt.wantTrustedSubnet {
+				t.Errorf("GetServerConfig() gotTrustedSubnet = %v, want %v", gotTrustedSubnet, tt.wantTrustedSubnet)
 			}
 		})
 	}
